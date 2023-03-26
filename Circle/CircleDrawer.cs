5cdc41d48a392c6eb370efccbbeebc0519aa865a
Ahmet ÖK <77897859+Ahmetok28@users.noreply.github.com>
2023-03-27 00:06:28 +0300

    circle

---
+++ Circle/CircleDrawer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class CircleDrawer
+    {
+        public  void DrawCircle(int radius)
+        {
+            for (int y = -radius; y <= radius; y++)
+            {
+                for (int x = -radius; x <= radius; x++)
+                {
+                    if (x * x + y * y <= radius * radius)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+
+}
---
+++ Circle/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Circle;
+
+Console.WriteLine("Lütfen çizmek istediğiniz dairenin yarı çapını yazınız:");
+
+int radius= Convert.ToInt32(Console.ReadLine());
+
+CircleDrawer drawer= new CircleDrawer();
+drawer.DrawCircle(radius);
