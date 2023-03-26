f71a33d4d81c330ab04b294d15dcf58f5e1ef87d
Ahmet ÖK <77897859+Ahmetok28@users.noreply.github.com>
2023-03-27 00:34:51 +0300

    reverse string

---
+++ SrtingReverse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Lüften bir metin giriniz: ");
+string input = Console.ReadLine();
+string output = "";
+string[] strings = input.Split(" ");
+
+foreach (var word in strings)
+{
+    char[] chars = word.ToCharArray();
+    Array.Reverse(chars);
+    string reverseWord = new string(chars);
+    output += reverseWord + " "; 
+}
+Console.Write("Output: "+output);
