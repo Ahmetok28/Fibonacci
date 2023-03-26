deca35617c64876fb6f2a944738d61e086e86535
Ahmet ÖK <77897859+Ahmetok28@users.noreply.github.com>
2023-03-27 00:13:54 +0300

    algortima

---
+++ Algoritma/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+    Console.Write("String ve index değerini virgülle ayırarak giriniz ");
+    string input = Console.ReadLine();
+
+    string[] inputArray = input.Split(',');
+    string str = inputArray[0].Trim();
+    int index;
+    bool success = int.TryParse(inputArray[1].Trim(), out index);
+
+    if (success && index >= 0 && index < str.Length)
+    {
+        Console.WriteLine("Sonuç: " + str.Remove(index, 1));
+    }
+    else
+    {
+        Console.WriteLine("Hatalı girdi, tekrar deneyiniz.");
+    }
