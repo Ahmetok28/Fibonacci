3f42876536f63b739d98494be75362008e325e80
Ahmet ÖK <77897859+Ahmetok28@users.noreply.github.com>
2023-03-28 02:13:12 +0300

    consonants

---
+++ Consonant/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Lüften bir metin giriniz: ");
+string input = Console.ReadLine();
+
+string[] words = input.Split(" ");
+
+foreach (var word in words)
+{
+    
+    Console.Write(CheckConsonants(word)+" ");
+}
+
+
+
+static bool CheckConsonants(string input)
+{
+    string consonants = "bcdfghjklmnpqrstvwxyz";
+    for (int i = 0; i < input.Length - 1; i++)
+    {
+        if (consonants.Contains(input[i]) && consonants.Contains(input[i + 1]))
+        {
+            return true;
+        }
+    }
+    return false;
+}
